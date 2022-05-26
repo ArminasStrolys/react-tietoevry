c745249caf15859974ba610cc8dd28aa4c490285
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-05-26 21:50:48 +0300

    Styling

--- src/components/dish/dish.jsx
+++ src/components/dish/dish.jsx
@@ -4,11 +4,13 @@ const Dish = (props) => {
   return (
     <div>
       <h2>{props.title}</h2>
+      <div className="info-card">
       <p>Serving portion: {props.serving}g</p>
       <p>Calories: {props.calories}g</p>
       <p>Carbohydrates: {props.carbs}g</p>
       <p>Fats: {props.fats}g</p>
       <p>Protein: {props.protein}g</p>
+      </div>
     </div>
   );
 };
