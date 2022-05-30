66dbb39c98615b7594e8d9901d5a29dda47ee227
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-05-30 21:13:01 +0300

    Update dishes.jsx

--- src/components/dishes/dishes.jsx
+++ src/components/dishes/dishes.jsx
@@ -1,19 +1,16 @@
-import React, { useState } from 'react';
-import Dish from "../dish/dish";
-import data from "../../data/data.json";
+import React, { useState } from "react";
 import Search from "../search/Search";
 
 const Dishes = () => {
-
-const [getCalories, setGetCalories] = useState('')
-const getCals = (data) => {
-    setGetCalories(data)
-}
+  const [getCalories, setGetCalories] = useState("");
+  const getCals = (data) => {
+    setGetCalories(data);
+  };
 
   return (
     <div>
-    <Search getCals={getCals}/>
-    { getCalories > 0 ? console.log('not null') : console.log('null') }
+      <Search getCals={getCals} />
+      {getCalories > 0 ? console.log("not null") : console.log("null")}
     </div>
   );
 };
