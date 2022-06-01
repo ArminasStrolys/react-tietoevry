8fbd2f8c0288b6418267016e2bbc3cdee82800d0
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-06-01 10:07:46 +0300

    Snack tab mapping

--- src/components/partyIdeas/PartyIdeas.jsx
+++ src/components/partyIdeas/PartyIdeas.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from "react";
+import data from "../../data/data.json";
+import Dish from "../dish/dish";
 
 export default function PartyIdeas() {
   return (
-    <div>Party meal idea tab
+    <div>
+     <h2>Snack ideas for a party</h2>
+      {data.map(
+        (item) =>
+          item.isSnack === true && (
+            <Dish
+              serving={item.servingSize}
+              title={item.title}
+              key={item.id}
+              calories={item.nutrition.calories}
+              fats={item.nutrition.fats}
+              carbs={item.nutrition.carbs}
+              protein={item.nutrition.protein}
+              breakfast={item.isBreakfast}
+              dinner={item.isDinner}
+              supper={item.isSupper}
+              snack={item.isSnack}
+            />
+          )
+      )}
     </div>
-  )
+  );
 }
