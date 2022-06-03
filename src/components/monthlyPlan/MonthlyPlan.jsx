79e255776add83676064fea03b9901b226ad5a76
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-06-03 09:13:40 +0300

    Tidying up the code

--- src/components/monthlyPlan/MonthlyPlan.jsx
+++ src/components/monthlyPlan/MonthlyPlan.jsx
@@ -1,38 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import Search from "../search/Search";
 import data from "../../data/data.json";
+import Dish from "../dish/dish";
 
 export default function MonthlyPlan() {
-  // const [getCalories, setGetCalories] = useState("");
-  // const getCals = (data) => {
-  //   setGetCalories(data);
-  // };
-
   function rndNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
-
-  // let sum = 0;
-  // const weeklyPlan = [];
-  // const ids = []
-
-  // rndNum(0,data.length)
-  // console.log(data[0].nutrition.calories)
-  // while (sum >= getCalories){
-  //   weeklyPlan.push(10)
-  //   sum = weeklyPlan.reduce((a,b)=>a+b)
-  //   console.log(weeklyPlan)
-  // }
-
-  // while (getCalories > sum) {
-  //   let x = rndNum(0, data.length - 1)
-  //   weeklyPlan.push(data[x].nutrition.calories);
-  //   ids.push(x)
-  //   sum = Math.round(weeklyPlan.reduce((a, b) => a + b));
-  //   console.log(weeklyPlan);
-  //   console.log('sum',sum);
-  // }
-
 
   const plan = [];
   const week = 7;
@@ -77,16 +51,36 @@ export default function MonthlyPlan() {
 
   console.log(plan);
 
+  const handleSmth = () => {
+console.log('ttt')
+  }
+
   return (
     <div>
-      {/* <Search getCals={getCals} /> */}
       <Search />
       Generate a meal plan for 1 week
-      <div>{}</div>
+      <div>{plan.map((item) =>
+              <Dish
+                serving={item[2]}
+                title={item[1]}
+                key={item[0]}
+                calories={item[3].calories}
+                fats={item[3].fats}
+                carbs={item[3].carbs}
+                protein={item[3].protein}
+                breakfast={item[4]}
+                dinner={item[5]}
+                supper={item[6]}
+                snack={item[7]}
+              />
+            )
+       }</div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <button
+          onClick={handleSmth}>
+          GEN
+        </button>
+      </form>
     </div>
   );
 }
-
-//4 breakfast
-//5 dinner
-//6 supper
